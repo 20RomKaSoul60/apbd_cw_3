2d3a50534a310d55a8e3c5e2aa70134f0459c204
20RomKaSoul <s32880@pjwstk.edu.pl>
2026-03-24 16:25:00 +0100

    Modification of class Borrowing.cs

--- APBD3/APBD3/Borrowing.cs
+++ APBD3/APBD3/Borrowing.cs
@@ -39,4 +39,45 @@ public class Borrowing
         }
     }
     
+    
+
+    private bool need_a_punishment
+    {
+        get
+        {
+            return this.need_a_punishment;
+        }
+        set { this.need_a_punishment = value; }
+    }
+
+    private bool need_a_debt
+    {
+        get { return this.need_a_debt; }
+        set { this.need_a_debt = value; }
+
+    }
+    
+
+    public void punishment()
+    {
+        if (moment_when_gave_back != moment_to)
+        {
+            if (int.Parse(moment_when_gave_back.Split('/')[0]) > int.Parse(moment_to.Split('/')[0]))
+            {
+                if (int.Parse(moment_when_gave_back.Split('/')[1]) == int.Parse(moment_to.Split('/')[1]))
+                {
+                    need_a_debt = true;
+                }
+                else
+                {
+                    need_a_debt = true;
+                    need_a_punishment = true;
+                }
+
+                
+            }
+        }
+    }
+
+
 }
