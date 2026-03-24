509a2c4c9f96f0e39703e7848a40a9d7aee21256
s32880 <s32880@pjwstk.edu.pl>
2026-03-24 23:15:01 +0100

    Third modification of class Borrowing.cs

--- APBD3/APBD3/Borrowing.cs
+++ APBD3/APBD3/Borrowing.cs
@@ -28,33 +28,59 @@ public class Borrowing
         set { this.moment_to = value; }
     }
 
-    private string moment_when_gave_back
+    private string moment_when_given_back
     {
         get
         {
-            return this.moment_when_gave_back;
+            return this.moment_when_given_back;
+        }
+        set
+        {
+            this.moment_when_given_back = value;
         }
     }
 
-    private bool isGivenBack
+    private bool on_time
+    {
+        get { return this.on_time; }
+        set { this.on_time = value; }
+    }
+    
+    private bool is_given_back
     {
         get
         {
-            return this.isGivenBack;
+            return this.is_given_back;
         }
 
         set
         {
-            this.isGivenBack = value;
+            this.is_given_back = value;
+            if (value == true)
+            {
+                if (this.moment_to == this.moment_when_given_back)
+                {
+                    this.on_time = true;
+                }
+                else
+                {
+                    this.on_time = false;
+                }
+            }
         }
     }
+
     
+
     public Borrowing(long borrower, long device,string moment_from,string moment_to)
     {
         this.id_of_borrower = borrower;
         this.id_of_device = device;
         this.moment_from = moment_from;
-        this.moment_to = moment_to;
+        this.moment_to = moment_from.Split('/')[0].Trim()
+                         +string.Join('/', int.Parse(moment_from.Split('/')[1].Trim())+1,'/')
+                         +moment_from.Split('/')[2].Trim();
+        this.moment_when_given_back = moment_to; 
 
     }
     
@@ -79,11 +105,11 @@ public class Borrowing
 
     public void punishment()
     {
-        if (moment_when_gave_back != moment_to)
+        if (!on_time)
         {
-            if (int.Parse(moment_when_gave_back.Split('/')[0]) > int.Parse(moment_to.Split('/')[0]))
+            if (int.Parse(moment_when_given_back.Split('/')[0]) > int.Parse(moment_to.Split('/')[0]))
             {
-                if (int.Parse(moment_when_gave_back.Split('/')[1]) == int.Parse(moment_to.Split('/')[1]))
+                if (int.Parse(moment_when_given_back.Split('/')[1]) == int.Parse(moment_to.Split('/')[1]))
                 {
                     need_a_debt = true;
                 }
